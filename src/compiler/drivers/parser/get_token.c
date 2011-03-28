eed393ed7a7a019d7747e29372ac362211b6f6be
Eudis Duran <eudisd@live.com>
2011-03-28 11:21:29 -0400

    Finished the "get_token" driver that works with file streams.
    So, the usual flow would be that I get the outputted token stream
    file, and the open it.  And as I process a token, I call get_token
    which simply extracts the next one.  Now I need to write two more
    functions to get the attribute value parameters.

---
+++ src/compiler/drivers/parser/get_token.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char *get_lexeme(FILE *i)
+char *get_valid_id(char *lexeme);
+char *get_token_name(char *lexeme);
+char *get_token(char *lexeme);
+
+int main()
+{
+    return 0;
+}
+
+char *get_token(char *lexeme)
+{
+    return NULL;
+}
+
+char *get_lexeme(FILE *i)
+{
+	return NULL;
+}
+
+char *get_valid_id(char *lexeme)
+{
+	return NULL;
+}
+
+char *get_token_name(char *lexeme)
+{
+	return NULL;
+}
